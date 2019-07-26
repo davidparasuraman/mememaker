937ddd86c57389ebbf7ee86f13cad151c8b9ffad
davidparasuram <davidparasuram@gmail.com>
2019-07-27 00:24:58 +0530

    MemeMaker setup

---
+++ app/src/main/java/com/david/mememaker/opoc/util/Callback.java
@@ -0,0 +1,34 @@
+/*#######################################################
+ *
+ *   Maintained by Gregor Santner, 2018-
+ *   https://gsantner.net/
+ *
+ *   License of this file: Apache 2.0 (Commercial upon request)
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *     https://github.com/gsantner/opoc/#licensing
+ *
+#########################################################*/
+package com.david.mememaker.opoc.util;
+
+@SuppressWarnings("unused")
+public class Callback {
+    public interface a1<A> {
+        void callback(A arg1);
+    }
+
+    public interface a2<A, B> {
+        void callback(A arg1, B arg2);
+    }
+
+    public interface a3<A, B, C> {
+        void callback(A arg1, B arg2, C arg3);
+    }
+
+    public interface a4<A, B, C, D> {
+        void callback(A arg1, B arg2, C arg3, D arg4);
+    }
+
+    public interface a5<A, B, C, D, E> {
+        void callback(A arg1, B arg2, C arg3, D arg4, E arg5);
+    }
+}
